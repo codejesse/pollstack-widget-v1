6ead2342c8ae95190d7b097be8f29d31f3697c62
Jesse <jessebeke10@gmail.com>
2024-10-15 00:32:13 +0100

    feat: added tailwindcss and shadcn for styling

---
+++ src/components/Widget.jsx
@@ -0,0 +1,7 @@
+import {Button} from "@/components/ui/button.jsx";
+
+export const Widget = () => {
+    return (
+        <Button className="fixed bottom-4 right-4 z-50">Give us feedback</Button>
+    )
+}
